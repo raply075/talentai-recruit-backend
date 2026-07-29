067843e4ccef490f154b67983ba9c1dc5689cbf0
Raply075 <raply060104@gmail.com>
2026-07-29 13:29:35 +0700

    fix: add cors configuration

---
+++ config/cors.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://talentai-recruit-frontend.vercel.app',
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
